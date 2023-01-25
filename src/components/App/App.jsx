dd5550d4aa8a6c415d82c3604a22391252d9733d
Sergey <smisyuk@gmail.com>
2023-01-25 02:05:11 +0200

    make save on localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-// import { useState, useEffect } from 'react';
 import { ContactForm } from 'components/ContactForm';
 import { ListContacts } from 'components/ListContacts';
 import { Title } from 'components/Title';
@@ -7,23 +6,9 @@ import { Phonebook, MainTitle } from "./App.styled"
 import { useSelector } from "react-redux";
 import { getContacts, getFilter } from 'redux/selectors';
 
-//custom hook
-// const useLocalStorage = (key, defaultValue) => {
-//   const [state, setState] = useState(() => {
-//     return JSON.parse(localStorage.getItem(key)) ?? defaultValue
-//   })
-
-//   useEffect(() => {
-//     localStorage.setItem(key, JSON.stringify(state))
-//   },[key, state])
-
-//   return [state, setState]
-// }
-
 export const App =()=>{
   const contacts = useSelector(getContacts)
   const filterValue = useSelector(getFilter)
-  // const [contacts, setContact] = useLocalStorage('contacts', [...contacts])
 
   const showContacts = () => {
     const fullBaseContacts = contacts;
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const CONTACTS = 'contacts'
+
 const INITIAL_CONTACTS = [
     { id: 'id-1', name: 'Тарас Шевченко', number: '459-12-56' },
-    {id: 'id-2', name: 'Іван Франко', number: '443-89-12'},
-    {id: 'id-3', name: 'Леся Українка', number: '645-17-79'},
+    { id: 'id-2', name: 'Іван Франко', number: '443-89-12'},
+    { id: 'id-3', name: 'Леся Українка', number: '645-17-79'},
     { id: 'id-4', name: 'Григорій Сковорода', number: '227-88-33' },
     { id: 'id-5', name: 'Ліна Костенко', number: '4567-78-26' },
     { id: 'id-6', name: 'Валер’ян Підмогильний', number: '527272-91-00' },
@@ -14,18 +16,20 @@ const INITIAL_CONTACTS = [
   ]
 
 const contactsSlice = createSlice({
-    name: "contacts",
-    initialState: INITIAL_CONTACTS,
+    name: CONTACTS,
+    initialState: JSON.parse(localStorage.getItem(CONTACTS)) ?? INITIAL_CONTACTS, 
     reducers: {
         addContact: {
             reducer(state, action) {
                 state.push(action.payload);
+                localStorage.setItem(CONTACTS, JSON.stringify(state))
             },          
         },
         removeContact: {
             reducer(state, action) {
                 const index = state.findIndex(task => task.id === action.payload);
                 state.splice(index, 1);
+                localStorage.setItem(CONTACTS, JSON.stringify(state))
             },            
         },
     },
@@ -33,3 +37,10 @@ const contactsSlice = createSlice({
 
 export const { addContact, removeContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
+
+
+//  JSON.parse(localStorage.getItem(key)) ?? defaultValue
+
+  
+
+//  localStorage.setItem(key, JSON.stringify(state))
